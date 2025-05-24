67c77fb7923ab599e7a45268263608c8d49fc8ac
skyfallen- <kaernick@yandex.ru>
2025-05-24 14:28:48 +0300

    Внедрил делеагат и вынес алерт модель  в отдельный файл

---
+++ MovieQuiz/Helpers/Keys+enum.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+private enum Keys: String {
+        case correctAnswers
+        case totalQuestions
+        case gamesCount
+        case bestGameCorrect = "bestGame.correct"
+        case bestGameTotal   = "bestGame.total"
+        case bestGameDate    = "bestGame.date"
+    }
